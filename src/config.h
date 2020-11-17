54a49abf7117d7b1ab5785f2f377ea1f21bdb13c
koonix <ehsangn@protonmail.ch>
2020-11-18 02:38:47 +0330

    Imported some of the configuration

--- src/config.h
+++ src/config.h
@@ -32,30 +32,26 @@
 // = Input/Output
 // =====================
 /* Put meaningful names on I/O pins */
-#define BUTTON   D,3
-#define LED_DOWN D,2
-#define LED_UP   D,4
+#define RELAY    D,5
 
 #ifdef MAIN_C
 static void io_init(void)
 {
-    INPUT  (BUTTON);
-    PULLUP (BUTTON);
-
-    OUTPUT (LED_UP);
-    OFF    (LED_UP);
-
-    OUTPUT (LED_DOWN);
-    ON     (LED_DOWN);
+    OUTPUT (RELAY);
+    OFF    (RELAY);
 }
 #endif
+
+/* Other bit manipulation macros */
+#define ADC_START_CONVERSION SETBIT(ADCSRA,ADSC)
+#define ADC_CONVERSION_IN_PROGRESS GETBIT(ADCSRA, ADSC)
 
 
 // =====================
 // = Other Config
 // =====================
 #define F_CPU 8000000UL /* avr clock speed (in HZ) */
-#define TASK_TIME_INTERVAL_MSEC 4 /* how often the task_time_manager runs (in milliseconds) */
+#define TASK_TIME_INTERVAL_MSEC 1 /* how often the tsk_task_time_manager runs (in milliseconds) */
 
 
 #ifdef TASKER_C
@@ -71,20 +67,10 @@ Task task_array[] = {
  * You should always set task's counter to 1.
  */
 
-/*    task function               state     period     counter  */
-    { blink_upper,                PAUSED,   MSEC(4),   1 },
-    { debounce,                 RUNNABLE,   MSEC(4),   1 },
-    { motor_startup,              PAUSED,   MSEC(20),  1 },
-    { motor_shutdown,             PAUSED,   MSEC(36),  1 },
-    { motor_rampup,               PAUSED,   MSEC(36),  1 },
-    { motor_rampdown,             PAUSED,   MSEC(36),  1 },
-    { motor_toggle_speed_control, PAUSED,   MSEC(24),  1 },
-    { button_event_handler,       PAUSED,   MSEC(4),   1 },
-    { blink,                      PAUSED,   MSEC(8),   1 },
-    { blink_upper,                PAUSED,   MSEC(32),  1 },
-    { blink_lower,                PAUSED,   MSEC(4),   1 },
-    { blink_upper_secondary,      PAUSED,   MSEC(4),   1 },
-    { blink_lower_secondary,      PAUSED,   MSEC(4),   1 },
+/*    task function      state     period     counter  */
+    { chkadc,            PAUSED,   MSEC(100),   1 },
+    { blink,             RUNNABLE, MSEC(100),   1 },
+    { blink_start,       PAUSED,   MSEC(200),   1 },
 };
 
 
@@ -104,38 +90,11 @@ Task task_array[] = {
 static void timer0_init(void)
 {
     TCCR0 =
-    (1 << CS02) |
-    (0 << CS01) |
-    (0 << CS00);
+    (0 << CS02) |
+    (1 << CS01) |
+    (1 << CS00);
 
     TCNT0 = TCNT0_VALUE;
-}
-
-static void timer1_init(void)
-{
-    TCCR1A =
-    (1 << COM1A1) |
-    (1 << COM1A0) |
-    (0 << COM1B1) |
-    (0 << COM1B0) |
-    (0 << FOC1A)  |
-    (1 << FOC1B)  |
-    (0 << WGM11)  |
-    (0 << WGM10);
-
-    TCCR1B =
-    (0 << ICNC1) |
-    (0 << ICES1) |
-    (1 << WGM13) |
-    (0 << WGM12) |
-    (0 << CS12)  |
-    (0 << CS11)  |
-    (1 << CS10);
-
-    TCNT1 = 0;
-    ICR1  = 100; /* TOP = 100; */
-    OCR1A = 0;
-    OCR1B = 0;
 }
 
 static void timer_interrupts_init(void)
@@ -181,7 +140,6 @@ static void registers_init(void)
 {
     io_init();
     timer0_init();
-    timer1_init();
     adc_init();
     timer_interrupts_init();
 }
