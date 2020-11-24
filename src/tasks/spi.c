8b6007413097c18887848a5ed32d57b97ae9080e
koonix <ehsangn@protonmail.ch>
2020-11-24 12:56:25 +0330

    A simple version of spi task is complete

--- src/tasks/spi.c
+++ src/tasks/spi.c
@@ -1,12 +1,55 @@
-#include <stdint.h>
 #include <avr/io.h>
-#include "iomacros.h"
+#include <stdint.h>
+#include <string.h>
 #include "spi.h"
+#include "config.h"
 
-#define IS_SPI_BUSY (GETBIT(SPSR,SPIF))
+/* local function prototypes */
+static uint8_t is_spi_busy(void);
 
-uint8_t spi(uint8_t data)
+/* main spi buffer for sending */
+char spi_send_buffer[SPI_BUFFSIZE] = {0};
+
+/* this is the user function for all spi communication. */
+uint8_t spi_send(const char* string)
 {
-    SPDR = data;
-    return SPDR;
+    if (is_spi_busy() == SPI_IS_BUSY)
+        return SPI_IS_BUSY;
+    strcpy(spi_send_buffer, string);
+    return SPI_IS_FREE;
+}
+
+/* the scheduled task that sends the string over spi one-byte at a time. */
+void spi_send_task(void)
+{
+    static uint8_t cycle = 0;
+
+    SPI_START;
+    SPDR = spi_send_buffer[cycle++];
+
+    if (cycle >= SPI_BUFFSIZE) {
+        cycle = 0;
+        task_set_state(spi_send_task, PAUSED);
+        task_set_state(spi_finish_transmission, RUNNABLE);
+    }
+}
+
+/* this runs once after data sending is done. */
+void spi_finish_transmission(void)
+{
+    SPI_END;
+    task_set_state(spi_finish_transmission, PAUSED);
+}
+
+/* detect if spi is busy transmitting */
+static uint8_t is_spi_busy(void)
+{
+    /* get task states */
+    TaskState send = task_get_state(spi_send_task);
+    TaskState fnsh = task_get_state(spi_finish_transmission);
+
+    /* exit this function right here if any spi transmission is in progress */
+    if (send == RUNNABLE || send == READY || fnsh == RUNNABLE || fnsh == READY)
+        return SPI_IS_BUSY;
+    return SPI_IS_FREE;
 }
---
+++ src/tasks/spi.h
@@ -0,0 +1,19 @@
+#ifndef SPI_H
+#define SPI_H
+
+#include <stdint.h>
+
+/* size of spi buffer string */
+#define SPI_BUFFSIZE 30
+
+/* the value that spi_send function returns if spi is busy */
+#define SPI_IS_BUSY 1
+#define SPI_IS_FREE 0
+
+
+uint8_t spi_send(const char* string);
+void spi_send_task(void);
+void spi_finish_transmission(void);
+void spi_align(void);
+
+#endif /* SPI_H */
